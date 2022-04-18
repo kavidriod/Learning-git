0646469f057cf6ed5cbc3bd03ec7d21d57619082
Kavitha <kavita.manikumar@gmail.com>
2022-04-18 07:21:11 +0530

    Initial Commit

---
+++ 2lesson_GitHubinXcode/_lesson_GitHubinXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _lesson_GitHubinXcodeApp.swift
+//  2lesson_GitHubinXcode
+//
+//  Created by Kavitha Nagarajan on 18/04/22.
+//
+
+import SwiftUI
+
+@main
+struct _lesson_GitHubinXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
